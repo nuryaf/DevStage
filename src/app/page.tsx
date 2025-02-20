422d641b964e40570d262b77a4747ea0143125b4
Nurya Fernandes <nurya.fernandes@outlook.com>
2025-02-19 22:00:37 -0300

    refactor(style-guide): aplicando o composition pattern no input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Mail } from 'lucide-react';
+import { InputField, InputIcon, InputRoot } from '@/components/input';
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className='size-5' />
+          </InputIcon>
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   );
 }
