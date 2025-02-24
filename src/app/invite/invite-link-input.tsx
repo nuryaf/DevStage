f64a4a73c93ac1683bf81ff5e8a9ea20b3c953a3
Nurya Fernandes <nurya.fernandes@outlook.com>
2025-02-23 21:36:33 -0300

    refactor: refatorando código do link de convite

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,18 +1,24 @@
+"use client";
+
 import IconButton from "@/components/icon-button";
 import { InputField, InputIcon, InputRoot } from "@/components/input";
 import { Copy, Link } from "lucide-react";
 
-export default function InviteLinkInput() {
+interface InviteLinkInputProps {
+	inviteLink: string;
+}
+
+export default function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+	const copyInviteLink = () => {
+		navigator.clipboard.writeText(inviteLink);
+	};
 	return (
 		<InputRoot>
 			<InputIcon>
 				<Link className="size-5" />
 			</InputIcon>
-			<InputField
-				readOnly
-				defaultValue="http://localhost:300/15a4s851d8asd4a52"
-			/>
-			<IconButton className="-mr-2">
+			<InputField readOnly defaultValue={inviteLink} />
+			<IconButton className="-mr-2" onClick={copyInviteLink}>
 				<Copy className="size-5" />
 			</IconButton>
 		</InputRoot>
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -6,6 +6,8 @@ import { Ranking } from "./ranking";
 import Stats from "./stats";
 
 export default function InvitePage() {
+	const inviteLink = "http://localhost:300/invite/15a4s851d8asd4a52";
+
 	return (
 		<div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
 			<div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -29,7 +31,7 @@ export default function InvitePage() {
 							inscrições:
 						</p>
 					</div>
-					<InviteLinkInput />
+					<InviteLinkInput inviteLink={inviteLink} />
 				</div>
 				<Stats />
 			</div>
