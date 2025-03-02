5009666fe0f9e2512146df2a9a6d07388a10d54a
Nurya Fernandes <nurya.fernandes@outlook.com>
2025-03-02 15:01:56 -0300

    feat(config): integrando o formulário com a API

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 
-import logo from "../../assets/logo.svg";
+import logo from "../../../assets/logo.svg";
 import InviteLinkInput from "./invite-link-input";
 import { Ranking } from "./ranking";
 import Stats from "./stats";
 
-export default function InvitePage() {
-	const inviteLink = "http://localhost:300/invite/15a4s851d8asd4a52";
+interface InvitePageProps {
+	params: Promise<{
+		subscriberId: string;
+	}>
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+	const { subscriberId } = await props.params
+
+	const inviteLink = `http://localhost:3333/invites/${subscriberId}`;
 
 	return (
 		<div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
